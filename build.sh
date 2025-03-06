aa75578249936d87b3f3bd35eb6b56040b04c80e
Damchey Lhendup <info@drukhost.com>
2025-03-06 22:15:30 +0600

    fix

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ if [ $# -eq 0 ]
     tag=$1
 fi
 echo 'Building Laravel PHP-FPM Image'
-docker build -f src/docker/Dockerfile-$tag -t drukhost/laravel-php-fpm:$tag PHP_VERSION=$tag .
+docker build -f src/docker/Dockerfile-$tag -t drukhost/laravel-php-fpm:$tag .
